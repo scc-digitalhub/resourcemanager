310ba97631a9f540ad50b34b3e7645e3c14af67e
Matteo Saloni <matteo.saloni@gmail.com>
2019-05-10 17:08:29 +0200

    add permission evaluation from authorities, static role - permission mapping

--- src/main/java/it/smartcommunitylab/resourcemanager/security/ScopePermissionEvaluator.java
+++ src/main/java/it/smartcommunitylab/resourcemanager/security/ScopePermissionEvaluator.java
@@ -2,7 +2,10 @@ package it.smartcommunitylab.resourcemanager.security;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
@@ -11,7 +14,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import it.smartcommunitylab.resourcemanager.SystemKeys;
 
 @Component
 public class ScopePermissionEvaluator implements PermissionEvaluator {
@@ -52,7 +58,12 @@ public class ScopePermissionEvaluator implements PermissionEvaluator {
 			Object permission) {
 
 		String scopeId = targetId.toString();
+		String userId = authentication.getName();
 		String action = permission.toString();
+
+		for (GrantedAuthority ga : authentication.getAuthorities()) {
+			_log.debug("user " + userId + " authority " + ga.toString());
+		}
 
 		boolean isPermitted = true;
 
@@ -60,13 +71,111 @@ public class ScopePermissionEvaluator implements PermissionEvaluator {
 			// check for scope in permitted
 			isPermitted = scopes.contains(scopeId);
 		}
-		_log.debug("hasPermission scope " + scopeId + " permitted " + isPermitted);
+		_log.debug("user " + userId + " hasPermission scope " + scopeId + " permitted " + isPermitted);
 
-		// TODO check in Auth!
-		boolean hasPermission = true;
-		_log.debug("hasPermission for scope " + scopeId + ":" + action + " " + hasPermission);
+		// check in Auth
+		boolean hasPermission = false;
+
+		// fetch realm AND scope roles
+		List<GrantedAuthority> authorities = new ArrayList<>(authentication.getAuthorities());
+		Set<String> roles = new HashSet<>();
+		roles.addAll(getRealmRoles(authorities));
+		roles.addAll(getScopeRoles(scopeId, authorities));
+
+		// fetch all permissions related to any assigned role
+		Set<String> permissions = new HashSet<>();
+		for (String role : roles) {
+			permissions.addAll(roleToPermissions(role));
+		}
+
+		_log.debug("user " + userId + " permissions " + permissions.toString());
+
+		hasPermission = permissions.contains(action);
+
+		_log.debug("user " + userId + " hasPermission for scope " + scopeId + ":" + action + " " + hasPermission);
 
 		return (isPermitted && hasPermission);
+	}
+
+	/*
+	 * Helpers
+	 */
+	private List<String> getRealmRoles(List<GrantedAuthority> authorities) {
+		List<String> roles = new ArrayList<>();
+
+		for (GrantedAuthority ga : authorities) {
+			// expected format [ROLE_*]
+			String auth = ga.getAuthority();
+			if (auth != null) {
+				if (auth.startsWith("ROLE_")) {
+					roles.add(auth);
+				}
+			}
+		}
+
+		return roles;
+	}
+
+	private List<String> getScopeRoles(String scopeId, List<GrantedAuthority> authorities) {
+		List<String> roles = new ArrayList<>();
+		String prefix = "/" + scopeId + "/";
+
+		for (GrantedAuthority ga : authorities) {
+			// expected format [scopeId]/[ROLE_*]
+			String auth = ga.getAuthority();
+			if (auth != null) {
+				if (auth.startsWith(prefix + "ROLE_")) {
+					roles.add(auth.replaceFirst(prefix, ""));
+				}
+			}
+		}
+
+		return roles;
+	}
+
+	private Set<String> roleToPermissions(String role) {
+		// statically resolve roles => permission mapping
+		// TODO refactor
+		Set<String> permissions = new HashSet<String>();
+
+		if (role.equals("ROLE_ADMIN")) {
+			permissions.addAll(Arrays.asList(
+					SystemKeys.PERMISSION_RESOURCE_CREATE,
+					SystemKeys.PERMISSION_RESOURCE_UPDATE,
+					SystemKeys.PERMISSION_RESOURCE_DELETE,
+					SystemKeys.PERMISSION_RESOURCE_CHECK,
+					SystemKeys.PERMISSION_RESOURCE_VIEW,
+
+					SystemKeys.PERMISSION_CONSUMER_CREATE,
+					SystemKeys.PERMISSION_CONSUMER_UPDATE,
+					SystemKeys.PERMISSION_CONSUMER_DELETE,
+					SystemKeys.PERMISSION_CONSUMER_VIEW));
+		} else if (role.equals("ROLE_RESOURCE_ADMIN")) {
+			permissions.addAll(Arrays.asList(
+					SystemKeys.PERMISSION_RESOURCE_CREATE,
+					SystemKeys.PERMISSION_RESOURCE_UPDATE,
+					SystemKeys.PERMISSION_RESOURCE_DELETE,
+					SystemKeys.PERMISSION_RESOURCE_CHECK,
+					SystemKeys.PERMISSION_RESOURCE_VIEW,
+
+					SystemKeys.PERMISSION_CONSUMER_VIEW));
+		} else if (role.equals("ROLE_CONSUMER_ADMIN")) {
+			permissions.addAll(Arrays.asList(
+					SystemKeys.PERMISSION_RESOURCE_CHECK,
+					SystemKeys.PERMISSION_RESOURCE_VIEW,
+
+					SystemKeys.PERMISSION_CONSUMER_CREATE,
+					SystemKeys.PERMISSION_CONSUMER_UPDATE,
+					SystemKeys.PERMISSION_CONSUMER_DELETE,
+					SystemKeys.PERMISSION_CONSUMER_VIEW));
+		} else if (role.equals("ROLE_USER")) {
+			permissions.addAll(Arrays.asList(
+					SystemKeys.PERMISSION_RESOURCE_CHECK,
+					SystemKeys.PERMISSION_RESOURCE_VIEW,
+					SystemKeys.PERMISSION_CONSUMER_VIEW));
+		}
+
+		return permissions;
 	}
 
 }
