cf0051dd96cb871c531229fea9ed4754fb81e7fe
Matteo Saloni <matteo.saloni@gmail.com>
2019-05-20 13:40:56 +0200

    add scope check to sqlPad consumer

--- src/main/java/it/smartcommunitylab/resourcemanager/consumer/sqlpad/SqlpadConsumer.java
+++ src/main/java/it/smartcommunitylab/resourcemanager/consumer/sqlpad/SqlpadConsumer.java
@@ -18,205 +18,222 @@ import it.smartcommunitylab.resourcemanager.util.SqlUtil;
 
 public class SqlpadConsumer extends Consumer {
 
-	private final static Logger _log = LoggerFactory.getLogger(SqlpadConsumer.class);
+    private final static Logger _log = LoggerFactory.getLogger(SqlpadConsumer.class);
 
-	public static final String TYPE = SystemKeys.TYPE_SQL;
-	public static final String ID = "sqlpad";
+    public static final String TYPE = SystemKeys.TYPE_SQL;
+    public static final String ID = "sqlpad";
 
-	// sqlpad connection
-	private String endpoint;
-	private String username;
-	private String password;
+    // sqlpad connection
+    private String endpoint;
+    private String username;
+    private String password;
 
-	private int STATUS;
+    private int STATUS;
 
-	private Registration registration;
+    private Registration registration;
 
-	private SqlpadClient _client;
+    private SqlpadClient _client;
 
-	public SqlpadConsumer() {
-		endpoint = "";
-		username = "";
-		password = "";
-	}
+    public SqlpadConsumer() {
+        endpoint = "";
+        username = "";
+        password = "";
+    }
 
-	public SqlpadConsumer(Map<String, Serializable> properties) {
-		this();
-		_properties = properties;
-	}
+    public SqlpadConsumer(Map<String, Serializable> properties) {
+        this();
+        _properties = properties;
+    }
 
-	public SqlpadConsumer(Registration reg) {
-		this();
-		registration = reg;
-		_properties = reg.getPropertiesMap();
-	}
+    public SqlpadConsumer(Registration reg) {
+        this();
+        registration = reg;
+        _properties = reg.getPropertiesMap();
+    }
 
-	// sqlpad properties
-	private Map<String, Serializable> _properties;
+    // sqlpad properties
+    private Map<String, Serializable> _properties;
 
-	public Map<String, Serializable> getProperties() {
-		return _properties;
-	}
+    public Map<String, Serializable> getProperties() {
+        return _properties;
+    }
 
-	@Override
-	public String getId() {
-		return ID;
-	}
+    @Override
+    public String getId() {
+        return ID;
+    }
 
-	@Override
-	public String getType() {
-		return TYPE;
-	}
+    @Override
+    public String getType() {
+        return TYPE;
+    }
 
-	@Override
-	public Registration getRegistration() {
-		return registration;
-	}
+    @Override
+    public Registration getRegistration() {
+        return registration;
+    }
 
-	/*
-	 * Init method - POST constructor since spring injects properties *after
-	 * creation*
-	 */
-	@PostConstruct
-	public void init() {
-		_log.debug("init called");
+    /*
+     * Init method - POST constructor since spring injects properties *after
+     * creation*
+     */
+    @PostConstruct
+    public void init() {
+        _log.debug("init called");
 
-		STATUS = SystemKeys.STATUS_UNKNOWN;
+        STATUS = SystemKeys.STATUS_UNKNOWN;
 
-		if (_properties != null) {
-			if (_properties.containsKey("endpoint")
-					&& _properties.containsKey("username")
-					&& _properties.containsKey("password")) {
+        if (_properties != null) {
+            if (_properties.containsKey("endpoint")
+                    && _properties.containsKey("username")
+                    && _properties.containsKey("password")) {
 
-				endpoint = _properties.get("endpoint").toString();
-				username = _properties.get("username").toString();
-				password = _properties.get("password").toString();
-			}
-		}
+                endpoint = _properties.get("endpoint").toString();
+                username = _properties.get("username").toString();
+                password = _properties.get("password").toString();
+            }
+        }
 
-		if (!endpoint.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
-			_client = new SqlpadClient(endpoint, username, password);
-			STATUS = SystemKeys.STATUS_READY;
-		}
-	}
+        if (!endpoint.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
+            _client = new SqlpadClient(endpoint, username, password);
+            STATUS = SystemKeys.STATUS_READY;
+        }
+    }
 
-	@Override
-	public int getStatus() {
-		return STATUS;
-	}
+    @Override
+    public int getStatus() {
+        return STATUS;
+    }
 
-	@Override
-	public void addResource(String scopeId, String userId, Resource resource) throws ConsumerException {
-		_log.debug("add resource " + resource.toString());
-		try {
-			// fetch provider driver from supported
-			String driver = getDriver(resource.getProvider());
-			if (!driver.isEmpty()) {
-				// supported
-				String uri = resource.getUri();
-				String host = SqlUtil.getHost(uri);
-				int port = SqlUtil.getPort(uri);
-				String uname = SqlUtil.getUsername(uri);
-				String passw = SqlUtil.getPassword(uri);
-				String database = SqlUtil.getDatabase(uri);
-				String name = driver + "_" + database;
+    @Override
+    public void addResource(String scopeId, String userId, Resource resource) throws ConsumerException {
+        if (checkScope(resource.getScopeId())) {
+            _log.debug("add resource " + resource.toString());
+            try {
+                // fetch provider driver from supported
+                String driver = getDriver(resource.getProvider());
+                if (!driver.isEmpty()) {
+                    // supported
+                    String uri = resource.getUri();
+                    String host = SqlUtil.getHost(uri);
+                    int port = SqlUtil.getPort(uri);
+                    String uname = SqlUtil.getUsername(uri);
+                    String passw = SqlUtil.getPassword(uri);
+                    String database = SqlUtil.getDatabase(uri);
+                    String name = driver + "_" + database;
 
-				String padId = _client.addConnection(driver, name, host, port, database, uname, passw);
+                    String padId = _client.addConnection(driver, name, host, port, database, uname, passw);
 
-				_log.debug("created pad " + padId);
-			}
-		} catch (SqlpadException e) {
-			_log.error("sqlpad error " + e.getMessage());
-			throw new ConsumerException(e.getMessage());
-		}
-	}
+                    _log.debug("created pad " + padId);
+                }
+            } catch (SqlpadException e) {
+                _log.error("sqlpad error " + e.getMessage());
+                throw new ConsumerException(e.getMessage());
+            }
+        }
+    }
 
-	@Override
-	public void updateResource(String scopeId, String userId, Resource resource) throws ConsumerException {
-		_log.debug("update resource " + resource.toString());
-		try {
-			// fetch provider driver from supported
-			String driver = getDriver(resource.getProvider());
-			if (!driver.isEmpty()) {
-				// supported
-				String uri = resource.getUri();
-				String host = SqlUtil.getHost(uri);
-				int port = SqlUtil.getPort(uri);
-				String uname = SqlUtil.getUsername(uri);
-				String passw = SqlUtil.getPassword(uri);
-				String database = SqlUtil.getDatabase(uri);
-				String name = driver + "_" + database;
+    @Override
+    public void updateResource(String scopeId, String userId, Resource resource) throws ConsumerException {
+        if (checkScope(resource.getScopeId())) {
+            _log.debug("update resource " + resource.toString());
+            try {
+                // fetch provider driver from supported
+                String driver = getDriver(resource.getProvider());
+                if (!driver.isEmpty()) {
+                    // supported
+                    String uri = resource.getUri();
+                    String host = SqlUtil.getHost(uri);
+                    int port = SqlUtil.getPort(uri);
+                    String uname = SqlUtil.getUsername(uri);
+                    String passw = SqlUtil.getPassword(uri);
+                    String database = SqlUtil.getDatabase(uri);
+                    String name = driver + "_" + database;
 
-				String padId = _client.updateConnection(driver, name, host, port, database, uname, passw);
+                    String padId = _client.updateConnection(driver, name, host, port, database, uname, passw);
 
-				_log.debug("updated pad " + padId);
-			}
-		} catch (SqlpadException e) {
-			_log.error("sqlpad error " + e.getMessage());
-			throw new ConsumerException(e.getMessage());
-		}
-	}
+                    _log.debug("updated pad " + padId);
+                }
+            } catch (SqlpadException e) {
+                _log.error("sqlpad error " + e.getMessage());
+                throw new ConsumerException(e.getMessage());
+            }
+        }
+    }
 
-	@Override
-	public void deleteResource(String scopeId, String userId, Resource resource) throws ConsumerException {
-		_log.debug("delete resource " + resource.toString());
-		try {
-			// fetch provider driver from supported
-			String driver = getDriver(resource.getProvider());
-			if (!driver.isEmpty()) {
-				// supported
-				String uri = resource.getUri();
-				String database = SqlUtil.getDatabase(uri);
-				String name = driver + "_" + database;
+    @Override
+    public void deleteResource(String scopeId, String userId, Resource resource) throws ConsumerException {
+        if (checkScope(resource.getScopeId())) {
+            _log.debug("delete resource " + resource.toString());
+            try {
+                // fetch provider driver from supported
+                String driver = getDriver(resource.getProvider());
+                if (!driver.isEmpty()) {
+                    // supported
+                    String uri = resource.getUri();
+                    String database = SqlUtil.getDatabase(uri);
+                    String name = driver + "_" + database;
 
-				String padId = _client.deleteConnection(driver, name);
-				if (!padId.isEmpty()) {
-					_log.debug("deleted pad " + padId);
-				}
-			}
-		} catch (SqlpadException e) {
-			_log.error("sqlpad error " + e.getMessage());
-			throw new ConsumerException(e.getMessage());
-		}
-	}
+                    String padId = _client.deleteConnection(driver, name);
+                    if (!padId.isEmpty()) {
+                        _log.debug("deleted pad " + padId);
+                    }
+                }
+            } catch (SqlpadException e) {
+                _log.error("sqlpad error " + e.getMessage());
+                throw new ConsumerException(e.getMessage());
+            }
+        }
+    }
 
-	@Override
-	public void checkResource(String scopeId, String userId, Resource resource) throws ConsumerException {
-		_log.debug("check resource " + resource.toString());
-		try {
-			// fetch provider driver from supported
-			String driver = getDriver(resource.getProvider());
-			if (!driver.isEmpty()) {
-				// supported
-				String uri = resource.getUri();
-				String database = SqlUtil.getDatabase(uri);
-				String name = driver + "_" + database;
+    @Override
+    public void checkResource(String scopeId, String userId, Resource resource) throws ConsumerException {
+        if (checkScope(resource.getScopeId())) {
+            _log.debug("check resource " + resource.toString());
+            try {
+                // fetch provider driver from supported
+                String driver = getDriver(resource.getProvider());
+                if (!driver.isEmpty()) {
+                    // supported
+                    String uri = resource.getUri();
+                    String database = SqlUtil.getDatabase(uri);
+                    String name = driver + "_" + database;
 
-				String padId = _client.hasConnection(driver, name);
+                    String padId = _client.hasConnection(driver, name);
 
-				if (!padId.isEmpty()) {
-					_log.debug("check ok pad " + padId);
-				}
-			}
-		} catch (SqlpadException e) {
-			_log.error("sqlpad error " + e.getMessage());
-			throw new ConsumerException(e.getMessage());
-		}
-	}
+                    if (!padId.isEmpty()) {
+                        _log.debug("check ok pad " + padId);
+                    }
+                }
+            } catch (SqlpadException e) {
+                _log.error("sqlpad error " + e.getMessage());
+                throw new ConsumerException(e.getMessage());
+            }
+        }
+    }
 
-	/*
-	 * Helpers
-	 */
-	public String getDriver(String provider) {
-		String driver = "";
-		switch (provider) {
-		case PostgresSqlProvider.ID:
-			driver = "postgres";
-			break;
-		case MySqlProvider.ID:
-			driver = "mysql";
-			break;
-		}
-		return driver;
-	}
+    /*
+     * Helpers
+     */
+    public String getDriver(String provider) {
+        String driver = "";
+        switch (provider) {
+        case PostgresSqlProvider.ID:
+            driver = "postgres";
+            break;
+        case MySqlProvider.ID:
+            driver = "mysql";
+            break;
+        }
+        return driver;
+    }
+
+    public boolean checkScope(String scope) {
+        if (this.registration != null) {
+            return registration.getScopeId().equals(scope);
+        } else {
+            // if global scope
+            return true;
+        }
+    }
 }
