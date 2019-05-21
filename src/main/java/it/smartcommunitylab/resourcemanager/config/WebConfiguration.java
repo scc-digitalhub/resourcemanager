ab1a3dd4cd69806a3cc519d44b13041d8b211047
Matteo Saloni <matteo.saloni@gmail.com>
2019-05-21 17:56:28 +0200

    add proper cors mapping for exposed custom headers

--- src/main/java/it/smartcommunitylab/resourcemanager/config/WebConfiguration.java
+++ src/main/java/it/smartcommunitylab/resourcemanager/config/WebConfiguration.java
@@ -7,8 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("Content-Type", "X-Total-Count")
+                .exposedHeaders("X-Total-Count");
+    }
 }
